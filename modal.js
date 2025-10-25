0e2a9183c373e1be60071370d626e7aaedc6b91d
tomhyhan <tomhyhan@gmail.com>
2025-10-25 16:10:09 -0700

    rendering modal ui && model logic

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ class App {
     // remove all elements in form container
     app.root.innerHTML = ""; 
     app.root.style.backgroundColor = "transparent";
+    app.root.style.padding = 0;
 
     // trigger glass wall effect
     const overlay = new Overlay()
@@ -70,19 +71,34 @@ class App {
   injectCSS = () => {
     const css = `
 /* todo: define color variables using root */
+:root {
+  /* colors */
+  --inactive: #ddd;
+  --progress-color: #568bda;
+  --button-color: #2d2d2d;
+  --button-bolder-color: #394e4b;
+
+  /* size */
+  --flex-small-gap: 5px;
+  --flex-middle-gap: 10px;
+  --border-width: 2px;
+
+}
 /* form button  */
 .form-btn {
   cursor: pointer;
   transition: all 0.1s linear;
-  border: 2px solid #2b2b2b;
+  border: var(--border-width) solid var(--button-bolder-color);
   border-radius: 5px;
   padding: 10px 15px;
   cursor: pointer;
-  box-shadow: 0 3px 2px 2px #4e4e4e;
+  box-shadow: 0 var(--border-width) 0px 0px #3b3b3b;
+  background-color: var(--button-color);
+  color: white;
 }
 
 .form-btn:hover {
-  filter: brightness(80%);
+  filter: brightness(90%);
 }
 
 .form-btn:focus, 
@@ -92,8 +108,8 @@ class App {
 }
 
 .form-btn:focus {
-  outline: 2px solid white;
-  border: 2px solid skyblue;
+  outline: var(--border-width) solid white;
+  border: var(--border-width) solid blue;
 }
 
 .form-btn:disabled {
@@ -103,7 +119,11 @@ class App {
 }
 
 .form-btn:active {
+  filter: brightness(80%);
+  border: var(--border-width) solid var(--button-bolder-color);
+  outline: none;
 }
+
 /* From Trigger */
 
 .form-overlay {
@@ -123,50 +143,46 @@ class App {
 }
 
 .form-page {
-  padding: 2rem;
+  max-width: 680px;
+  width: 100%;
+  padding: 3.5rem 2rem;
   background-color: white;
   position: relative;
   z-index: 10001;
 }
 
 .formTrigger {
-}
-
-.formTrigger__header {
-
+  display: flex;
+  flex-direction: column;
 }
 
 .formTrigger__p {
-  color: gray
-
+  color: gray;
+  margin-bottom: 3rem
 }
 
 .formTrigger__btn {
-  color: white;
-  background-color: black;
-  background-color: green;
+  align-self: flex-end;
 }
 
-
-/* form modal */
+/* form modal container */
 .form-modal-container {
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  padding: 2rem;
   background-color: white;
   border-radius: 8px;
-  box-shadow: 0 5px 15px rgba(0,0,0,0.3);
   z-index: 10003; /* Above form overlay */
-  width: 90%;
   max-width: 500px;
   animation: fadeIn 0.3s ease-out;
+  width: 100%;
 }
 
 .form-modal-container.remove {
   animation: fadeOut 0.3s ease-in forwards;
 }
-
 
 .form-modal__close {
   position: absolute;
@@ -181,14 +197,155 @@ class App {
   line-height: 1;
 }
 
+/* form modal progress bar */
+
+.form-progress-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start; /* Aligns circles */
+  margin: 1rem;
+  padding-top: 1rem; 
+}
+
+.form-progress-step {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  flex-basis: 0; /* Distribute space evenly */
+  flex-grow: 1;
+  color: var(--progress-color);
+}
+
+.form-progress-step span {
+  font-size: 0.8rem;
+  margin-top: 10px;
+  text-align: center;
+}
+
+/* The connecting line */
+.form-progress-step:not(:first-child)::before {
+  content: '';
+  position: absolute;
+  top: 20px; 
+  right: 50%;
+  width: 100%; 
+  height: 4px;
+  background-color: #ddd; /* Default inactive line */
+  z-index: 1; /* Behind the icon */
+}
+
+.form-progress-icon {
+  width: 40px;
+  height: 40px;
+  padding: 3px;
+  border-radius: 50%;
+  background-color: white;
+  border: var(--border-width) solid var(--inactive); /* Default inactive border */
+  color: var(--progress-color);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  z-index: 2; /* Above the line */
+  cursor: pointer;
+}
+
+.form-progress-badge {
+  display: none;
+  position: absolute;
+  top: -4px;
+  right: -4px;
+  width: 16px;
+  height: 16px;
+  background-color: #198754; 
+  border-radius: 50%;
+  border: 1px solid var(--inactive);
+  z-index: 3;
+  align-items: center;
+  justify-content: center;
+}
+
+.form-progress-badge::after {
+  content: '✔';
+  color: white;
+  font-size: 13px;
+  font-weight: bold;
+}
+
+/* Active Step */
+.form-progress-step.active {
+  color: var(--progress-color);
+  font-weight: bold;
+}
+.form-progress-step.active .form-progress-icon {
+  background-color: var(--progress-color);
+  border-color: var(--progress-color);
+  color: white;
+}
+
+/* Completed Step */
+.form-progress-step.completed .form-progress-icon {
+  background-color: var(--progress-color);
+  border-color: var(--progress-color);
+  color: white; 
+}
+.form-progress-step.completed::before, .form-progress-step.active::before{
+  background-color: var(--progress-color); 
+}
+.form-progress-step.completed .form-progress-badge {
+  display: flex; 
+}
+
+/* form modal */
+.form-modal {
+}
+
+.form-step {
+  display:flex;
+  flex-direction: column;
+  gap: var(--flex-middle-gap);
+}
+
+.form-step__name {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: var(--flex-small-gap);
+}
+
+.form-step__buttons {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: var(--flex-middle-gap)
+}
+
+
+.inputbox-group {
+  display: flex;
+  flex-direction: column;
+  gap: var(--flex-small-gap);
+}
+
+.inputbox-group__star {
+  color: red;
+}
+
 .form-modal__close:hover {
   color: #000;
 }
 
+.inputbox-group input,
+.inputbox-group textarea {
+  padding: 9px 10px;
+  border: var(--border-width) solid var(--inactive);
+}
+
 .form-modal .checkbox-group {
   display: flex;
-  align-items: flex-start;
-  margin: 0.5rem 0;
+  align-items: center;
+  font-size: small;
 }
 
 .form-modal .checkbox-group input[type="checkbox"] {
@@ -197,6 +354,7 @@ class App {
   -moz-appearance: auto;
   
   width: auto;
+  margin-right: 8px;
 }
 
 /* animation */
@@ -210,9 +368,14 @@ class App {
   to {opacity: 0;}
 }
 
-
-/* page break point: 768px */
-    `
+/* page break point: 768px */   
+@media (max-width: 768px) {
+  .form-step__name {
+    flex-direction: column;
+    align-items: start;
+  }
+}
+`
     const styleElement = document.createElement("style");
     styleElement.innerHTML = css;
     document.head.appendChild(styleElement);
@@ -244,39 +407,172 @@ class PageComponent extends BaseComponent {
 class FormTrigger extends BaseComponent {
   constructor() {
     super(`<section class="formTrigger">
-      <h4 class="formTrigger__header">Hello Conversion!</h4>
+      <h3 class="formTrigger__header">Hello Conversion!</h3>
       <p class="formTrigger__p">Click on the button below to contact us</p>
-      <button class="form-btn formTrigger__btn">Click here</button>
+      <button class="form-btn formTrigger__btn" disabled>Click here</button>
     </section>`)
   }
 }
 
 class Modal extends BaseComponent {
   constructor() {
-    super(`<div class="form-modal-container">
-            <button class="form-modal__close" aria-label="Close modal">&times;</button>      
-            <form class="form-modal">
-              <fieldset>
-                <div>
-                  <label for="fname">First name*</label>
-                  <input type="text" id="fname" name="fname" required>
-                  <label for="fname">Last name*</label>
-                  <input type="text" id="fname" name="fname" required>
-                </div>
-                <label for="fname">Last name*</label>
-                <input type="text" id="fname" name="fname" required>
-              </fieldset>
-              <fieldset>
-                <label for="help">How can we help you?</label>
-                <textarea id="help" name="help"></textarea>
-              <div class="checkbox-group">
-                <input type="checkbox" id="updates" name="updates">
-                <label for="updates">Yes, I would like to receive updates and other information from Conversion.</label>
+    super(`
+      <div class="form-modal-container">
+        <button class="form-modal__close" aria-label="Close modal">&times;</button>
+        
+        <div class="form-progress-bar">
+          <div class="form-progress-step" data-step="1">
+            <div class="form-progress-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A1.5 1.5 0 0 1 18 21.75H6a1.5 1.5 0 0 1-1.499-1.632Z" />
+              </svg>
+              <div class="form-progress-badge"></div>
+            </div>
+            <span>User Information</span>
+          </div>
+          <div class="form-progress-step" data-step="2">
+            <div class="form-progress-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+              </svg>
+              <div class="form-progress-badge"></div>
+            </div>
+            <span>Inquery</span>
+          </div>
+          <div class="form-progress-step" data-step="3">
+            <div class="form-progress-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L6 12Zm0 0h7.5" />
+              </svg>
+              <div class="form-progress-badge"></div>
+            </div>
+            <span>Complete</span>
+          </div>
+        </div>
+
+        <form class="form-modal">
+          <div class="form-step" data-step="1">
+            <div class="form-step__name">
+              <div class="inputbox-group">
+                <label for="fname">First name<span class="inputbox-group__star">*</span></label>
+                <input type="text" id="fname" name="fname" placeholder="First name" required>
               </div>
-                <button type="submit" class="form-btn modal-btn__submit">Get In Touch</button>
-              </fieldset>
-            </form>
-          </div>`)
+              <div class="inputbox-group">
+                <label for="lname">Last name</label>
+                <input type="text" id="lname" name="lname" placeholder="Last name">
+              </div>
+              </div>
+            <div class="inputbox-group">
+              <label for="email">Work Email<span class="inputbox-group__star">*</span></label>
+              <input type="email" id="email" name="email" required placeholder="Work Email">
+            </div>
+            <div class="form-step__buttons">
+              <button type="button" class="form-btn modal-btn__next">Next</button>
+            </div>
+          </div>
+      
+          <div class="form-step" data-step="2">
+            <div class="inputbox-group">
+              <label for="help">How can we help you?</label>
+              <textarea id="help" name="help" rows="4" placeholder="Tell us more about how we can help"></textarea>
+            </div>
+            <div class="checkbox-group">
+              <input type="checkbox" id="updates" name="updates">
+              <label for="updates">Yes, I would like to receive updates and other information from Conversion</label>
+            </div>
+            <div class="form-step__buttons">
+              <button type="button" class="form-btn modal-btn__back">Back</button>
+              <button type="submit" class="form-btn modal-btn__submit">Get In Touch</button>
+            </div>
+          </div>
+      
+          <div class="form-step" data-step="3">
+            <h4>Thank You!</h4>
+          </div>
+        </form>
+      </div>
+    `);
+
+    const modal = this;
+    modal.state = {
+      currentStep: 1,
+      maxStep:1
+    };
+    
+    modal.form = modal.element.querySelector(".form-modal");
+    modal.steps = modal.element.querySelectorAll(".form-step");
+    modal.progressSteps = modal.element.querySelectorAll(".form-progress-step");
+    
+    modal.addEventListeners();
+    modal.render();
+  }
+
+  goToStep = (targetStep) => {
+    const modal = this;
+    const currentStep = modal.state.currentStep;
+
+    if (targetStep > currentStep) {
+      if (!modal.validateStep(currentStep)) return;
+    }
+    modal.state.currentStep = targetStep;
+    modal.state.maxStep = Math.max(modal.state.maxStep, targetStep)
+    modal.render();
+  }
+  
+  render = () => {
+    const currentStepNum = this.state.currentStep;
+
+    this.steps.forEach(step => step.style.display = (step.dataset.step == currentStepNum) ? "flex" : "none");
+
+    this.progressSteps.forEach((progressStep, index) => {
+      const step = index + 1;
+
+      progressStep.classList.remove('active', 'completed');
+      if (step < currentStepNum) {
+        progressStep.classList.add('completed'); 
+      } else if (step === currentStepNum) {
+        progressStep.classList.add('active'); 
+      } 
+    });
+  }
+
+  addEventListeners = () => {
+    const modal = this
+    const nextBtn = modal.element.querySelector(".modal-btn__next");
+    nextBtn.addEventListener("click", () => modal.goToStep(2));
+
+    const backBtn = modal.element.querySelector(".modal-btn__back");
+    backBtn.addEventListener("click", () => modal.goToStep(1));
+
+    modal.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      modal.goToStep(3);
+    })
+
+    modal.progressSteps.forEach((progressStep, index) => {
+      progressStep.addEventListener('click', () => {
+        const targetStep = index + 1;
+        if (targetStep <= modal.state.maxStep) 
+          modal.goToStep(targetStep);
+      });
+    });
+  }
+
+  validateStep = (stepNum) => {
+    const currentStepEl = this.element.querySelector(`.form-step[data-step="${stepNum}"]`);
+    const inputs = currentStepEl.querySelectorAll("input[required], textarea[required]");
+    
+    let allValid = true;
+    for (const input of inputs) {
+      if (!input.checkValidity()) {
+        allValid = false;
+        console.warn(`Validation failed for: ${input.name}`);
+        alert(`Please fill out all required fields.\n'${input.labels[0].innerText}' is required.`);
+        input.focus();
+        break; 
+      }
+    }
+    return allValid;
   }
 }
 
