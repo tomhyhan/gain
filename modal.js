947cd090417240b4f016a2ab930923045732dbf5
tomhyhan <tomhyhan@gmail.com>
2025-10-25 11:38:52 -0700

    clicking form trigger button opens modal

--- modal.js
+++ modal.js
@@ -24,6 +24,8 @@ class App {
     const formTrigger = new FormTrigger()
     app.page.addChild(formTrigger)
 
+    const modal = new Modal()
+
     // clicking overlay removes it
     overlay.element.addEventListener("click", () => {
       overlay.element.classList.add("remove")
@@ -31,6 +33,30 @@ class App {
       setTimeout(() => {
         overlay.element.remove()
       }, 350)
+    })
+
+    const formTriggerBtn = document.querySelector(".formTrigger__btn")
+    formTriggerBtn.addEventListener("click", () => {
+      const overlay = new Overlay()
+      overlay.attachTo(document.body)
+      overlay.putAboveFormTrigger()
+
+      modal.element.classList.remove("remove");
+      modal.attachTo(document.body)
+
+      const closeModal = () => {
+        overlay.element.classList.add("remove")
+        modal.element.classList.add("remove")
+        
+        setTimeout(() => {
+          overlay.element.remove()
+          modal.element.remove() 
+        }, 350)
+      }
+
+      overlay.element.addEventListener("click", closeModal)
+
+      modal.element.querySelector(".form-modal__close").onclick = closeModal;
     })
 
     // navigate to from trigger when window size is lt 768
@@ -43,6 +69,7 @@ class App {
 
   injectCSS = () => {
     const css = `
+/* todo: define color variables using root */
 /* form button  */
 .form-btn {
   cursor: pointer;
@@ -121,6 +148,57 @@ class App {
 }
 
 
+/* form modal */
+.form-modal-container {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 5px 15px rgba(0,0,0,0.3);
+  z-index: 10003; /* Above form overlay */
+  width: 90%;
+  max-width: 500px;
+  animation: fadeIn 0.3s ease-out;
+}
+
+.form-modal-container.remove {
+  animation: fadeOut 0.3s ease-in forwards;
+}
+
+
+.form-modal__close {
+  position: absolute;
+  top: 10px;
+  right: 15px;
+  font-size: 2rem;
+  font-weight: bold;
+  color: #888;
+  background: none;
+  border: none;
+  cursor: pointer;
+  line-height: 1;
+}
+
+.form-modal__close:hover {
+  color: #000;
+}
+
+.form-modal .checkbox-group {
+  display: flex;
+  align-items: flex-start;
+  margin: 0.5rem 0;
+}
+
+.form-modal .checkbox-group input[type="checkbox"] {
+  appearance: auto;
+  -webkit-appearance: auto;
+  -moz-appearance: auto;
+  
+  width: auto;
+}
+
 /* animation */
 @keyframes fadeIn {
   from {opacity: 0;}
@@ -133,8 +211,7 @@ class App {
 }
 
 
-/* page break point: 768px */* page break point: 768px */
-
+/* page break point: 768px */
     `
     const styleElement = document.createElement("style");
     styleElement.innerHTML = css;
@@ -174,9 +251,46 @@ class FormTrigger extends BaseComponent {
   }
 }
 
+class Modal extends BaseComponent {
+  constructor() {
+    super(`<div class="form-modal-container">
+            <button class="form-modal__close" aria-label="Close modal">&times;</button>      
+            <form class="form-modal">
+              <fieldset>
+                <div>
+                  <label for="fname">First name*</label>
+                  <input type="text" id="fname" name="fname" required>
+                  <label for="fname">Last name*</label>
+                  <input type="text" id="fname" name="fname" required>
+                </div>
+                <label for="fname">Last name*</label>
+                <input type="text" id="fname" name="fname" required>
+              </fieldset>
+              <fieldset>
+                <label for="help">How can we help you?</label>
+                <textarea id="help" name="help"></textarea>
+              <div class="checkbox-group">
+                <input type="checkbox" id="updates" name="updates">
+                <label for="updates">Yes, I would like to receive updates and other information from Conversion.</label>
+              </div>
+                <button type="submit" class="form-btn modal-btn__submit">Get In Touch</button>
+              </fieldset>
+            </form>
+          </div>`)
+  }
+}
+
 class Overlay extends BaseComponent {
   constructor() {
     super("<div class='form-overlay'></div>")
+  }
+
+  putAboveFormTrigger = () => {
+    const overlay = this
+    const currentZIndex = window.getComputedStyle(overlay.element).zIndex;
+    const currentZNum = parseInt(currentZIndex, 10) || 0;
+
+    this.element.style.zIndex = currentZNum + 2;
   }
 }
 
